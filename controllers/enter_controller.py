44fdfa99f56039ff45e3790a813f5d04252e82d0
Timofey <nsk.timka2077@gmail.com>
2024-11-15 12:04:41 +0200

    add enter for admins

--- controllers/enter_controller.py
+++ controllers/enter_controller.py
@@ -10,8 +10,16 @@ class EnterController:
         self.view.ui.enterButton.clicked.connect(self.handle_enter_button)
 
     def handle_enter_button(self):
-        name = self.view.ui.loginEdit.text()
-        is_valid = self.model.validate(name)
-        print(is_valid)
-        if is_valid:
+        email = self.view.ui.emailEdit.text()
+        password = self.view.ui.passwordEdit.text()
+        is_valid, verdict = self.model.validate_fields(
+            email,
+            password,
+        )
+        response = self.model.check_admin_in_database(email, password)
+        if not is_valid:
+            self.view.ui.errorLabel.setText(verdict)
+        elif is_valid and not response:
+            self.view.ui.errorLabel.setText("админ не найден")
+        else:
             self.app.switch_view(1)
--- models/enter_model.py
+++ models/enter_model.py
@@ -1,9 +1,31 @@
+import sqlite3
+
+
 class EnterModel:
     def __init__(self, db_name="gym.db"):
         self.db_name = db_name
+        self.connect_to_database()
+
+    def connect_to_database(self):
+        self.connection = sqlite3.connect(self.db_name)
+        self.cursor = self.connection.cursor()
 
     @staticmethod
-    def validate(name):
-        if not name:
-            return False
-        return True
+    def validate_fields(email, password):
+        if not email and not password:
+            return (False, "поля должны быть заполнены")
+        if not email and password:
+            return (False, "поле email не может быть пустым")
+        if not password and email:
+            return (False, "поле password не может быть пустым")
+        return (True, "success")
+
+    def check_admin_in_database(self, email, password):
+        response = self.cursor.execute(
+            f"""
+                SELECT * FROM admins
+                WHERE email = '{email}'
+                AND password = '{password}'
+            """
+        ).fetchone()
+        return response
