aa743577a3732914d90cf80f1dfdde55db329b11
Timofey <nsk.timka2077@gmail.com>
2024-11-15 15:49:46 +0200

    add availability to add clients

--- controllers/add_client_controller.py
+++ controllers/add_client_controller.py
@@ -6,5 +6,19 @@ class AddClientController:
 
     def handle_create_client(self):
         title = self.view.ui.comboBoxMembership.currentText().split(" - ")
-        membership_id = self.model.get_membership_id_by_title(title)
-        print(membership_id)
+        membership_id = self.model.get_membership_id_by_title(title)[0]
+        name, surname, gender, email = (
+            self.view.ui.nameAddEdit.text(),
+            self.view.ui.surnameAddEdit.text(),
+            self.view.ui.genderAddEdit.text(),
+            self.view.ui.emailAddEdit.text(),
+        )
+        self.model.add_client(
+            name,
+            surname,
+            membership_id,
+            gender,
+            email,
+        )
+        self.view.parent().fill_table()
+        self.view.close()
